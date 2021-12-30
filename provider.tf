0a5ab003b23e7362c91fad8b93d26a1d063790b5
Jagadeesh B <jagadeeshb@datatemplate.com>
2021-12-30 12:41:22 +0530

    stack creation

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-#variables
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "compartment_ocid" {}
-variable "private_key_path" {}
-variable "fingerprint" {}
-variable "region" {}
-variable "ssh_public_key_path" {}
-variable "AD" {}
-variable "InstanceShape" {}
-
 provider "oci" {
   tenancy_ocid         = var.tenancy_ocid
   user_ocid            = var.user_ocid
